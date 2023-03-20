a976a7f1191736ff1f6be5f9a214a85a2ae7c78a
MikeFo5629 <122396385+MikeFo5629@users.noreply.github.com>
2023-03-20 15:39:23 -0400

    Added infinities, and NaNs
    Still missing overflow, underflow, and subnormals

--- multiplication/float_mult_16bit.sv
+++ multiplication/float_mult_16bit.sv
@@ -4,7 +4,8 @@ module half_FP_mult (
   output[15:0] product
 );
   
-  logic sign1, sign2, sign_p;
+  logic sign1, sign2, sign_p; 
+  logic hidden_lead1, hidden_lead2;
   logic [4:0] exp1, exp2, exp_p;
   logic [5:0] exp_comb;
   logic [9:0] mant1, mant2, mant_p;
@@ -14,6 +15,7 @@ module half_FP_mult (
   
   typedef enum logic [2:0] { 
     initializing,
+    special_case,
     multiply,
     normalize_mant,
     assemble
@@ -22,28 +24,70 @@ module half_FP_mult (
   state_t state;
   state_t next_state;
   
-  
   always_ff @(posedge clk) begin
     case(state)
       // Currently goes
-      // Initializing ->
-      // Multiplying ->
-      // Normalize_Mant
-      // Assemble
+      // initializing ->
+      // special_case ->
+      // multiplying ->
+      // normalize_mant ->
+      // assemble
       initializing: begin
-        sign1 = float1[15];
-        sign2 = float2[15];
-        exp1 = float1[14:10];
-        exp2 = float2[14:10];
-        mant1 = float1[9:0];
-        mant2 = float2[9:0];
-        next_state = multiply;
+        sign1 =       float1[15];
+        sign2 =       float2[15];
+    
+        exp1 =        float1[14:10];
+        exp2 =        float2[14:10];
+        
+        hidden_lead1 = 1'b0;
+        hidden_lead2 = 1'b0;
+        mant1 =       float1[9:0];
+        mant2 =       float2[9:0];
+        next_state =  special_case;
+      end
+      
+      special_case: begin
+        // if either number is infinity, output is infinity
+        if((exp1 == 5'b11111 & mant1 == 0) | (exp2 == 5'b11111 & mant2 == 0)) begin
+          sign_p =    sign1 ^ sign2; // Accounts for +/- infinity
+          exp_p =     5'b11111;
+          mant_p =    10'b0000000000;
+          next_state = assemble;
+        end
+        
+        // if either number is quiet NaN, return quiet Nan
+        else if((exp1 == 5'd31 & mant1[9] == 1'b0) | (exp2 == 5'd31 & mant2[9] == 1'b0)) begin
+          sign_p =    1'b1;
+          exp_p =     5'b11111;
+          mant_p =    10'b0111111111;
+          next_state = assemble;
+        end
+        
+        // if either number is signalling NaN, return signalling Nan
+        else if((exp1 == 5'd31 & mant1[9] == 1'b1) | (exp2 == 5'd31 & mant2[9] == 1'b1)) begin
+          sign_p =    1'b1;
+          exp_p =     5'b11111;
+          mant_p =    10'b1111111111;
+          next_state = assemble;
+        end
+        
+        // if either number is zero, return zero
+        else if((exp1 == 0 & mant1 == 0) | (exp2 == 0 & mant2 == 0)) begin
+          sign_p =    sign1 ^ sign2; // IEE754 mandates signed zero
+          exp_p =     5'b00000;
+          mant_p =    10'b0000000000;
+          next_state = assemble;
+        end
+        
+        else begin
+          next_state = multiply;
+        end
       end
       
       multiply: begin
         sign_p = sign1 ^ sign2;
         exp_p = exp1 + exp2 - 5'd15;
-        mant_multiplied = {1, mant1} * {1, mant2};
+        mant_multiplied = {hidden_lead1, mant1} * {hidden_lead2, mant2};
         
         next_state = normalize_mant;
       end
